1c144cd1ab952e2d277ae1d11504b23c60ef8dc8
rafiul hasan <rafiulhasan.bubt@gmail.com>
2024-02-05 09:33:07 +0600

    contact component added

---
+++ lib/contact_social.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ContactSocial extends StatelessWidget {
+  const ContactSocial({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
